cbe9c77bdf207a08197564194900145944345028
Ivan <ivann1922@gmail.com>
2018-02-26 14:51:24 -0600

    Página completa

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoService } from '../../services/info.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public infoservice: InfoService) { }
 
   ngOnInit() {
   }
--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoService } from '../../services/info.service';
 
 @Component({
   selector: 'app-tools',
